8d608dc7b55be4d94bb6532ede9f1688b8643c3b
Brian Fraser <bfraser@frasertechnicalsolutions.com>
2019-09-21 11:33:45 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,20 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `FTS Bookmarks`,
+        short_name: `FTS`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fae042`,
+        display: `standalone`,
+        icon: 'src/images/icon.png'
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
