08a635a3106ba5d4233992df7a0c0c0b51b66d58
Brian Fraser <bfraser@frasertechnicalsolutions.com>
2020-02-20 14:55:57 -0500

    updated layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,9 +4,12 @@ import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import ListItemText from '@material-ui/core/ListItemText';
 import { withStyles, WithStyles } from '@material-ui/core/styles';
 import { Breakpoint } from '@material-ui/core/styles/createBreakpoints';
@@ -15,14 +18,7 @@ import Typography from '@material-ui/core/Typography';
 import { createStyles, ThemeProvider } from '@material-ui/styles';
 import { Link } from 'gatsby';
 
-import {
-  Home,
-  Inbox,
-  Mail,
-  Search,
-  SelectAll,
-  Timeline,
-} from '@material-ui/icons';
+import { Home, Search } from '@material-ui/icons';
 
 import createMyTheme from '../styles/mainTheme';
 
@@ -56,19 +52,35 @@ const drawerWidth = 240;
 const styles = (theme: any) =>
   createStyles({
     appBar: {
-      marginLeft: drawerWidth,
-      width: `calc(100% - ${drawerWidth}px)`,
+      [theme.breakpoints.up('sm')]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+      },
+    },
+    leftMenu: {
+      marginTop: '80px',
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up('sm')]: {
+        display: 'none',
+      },
     },
     content: {
       backgroundColor: '#fff',
       flexGrow: 1,
-      marginLeft: drawerWidth,
       marginTop: '80px',
       padding: theme.spacing(3),
+      [theme.breakpoints.up('sm')]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+      },
     },
     drawer: {
-      flexShrink: 0,
-      width: drawerWidth,
+      [theme.breakpoints.up('sm')]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
     },
     drawerPaper: {
       width: drawerWidth,
@@ -85,6 +97,34 @@ interface IProps extends WithStyles<typeof styles> {
 
 const Layout: React.FC<IProps> = (props: IProps) => {
   const { classes } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <>
+      <List className={classes.leftMenu}>
+        <ListItem button={true} key="home">
+          <ListItemIcon>
+            <Home />
+          </ListItemIcon>
+          <Link to="/">
+            <ListItemText primary="Home" />
+          </Link>
+        </ListItem>
+        <ListItem button={true} key="searchbookmarks">
+          <ListItemIcon>
+            <Search />
+          </ListItemIcon>
+          <Link to="/Search">
+            <ListItemText primary="Search" />
+          </Link>
+        </ListItem>
+      </List>
+    </>
+  );
 
   return (
     <ThemeProvider theme={myTheme}>
@@ -98,38 +138,44 @@ const Layout: React.FC<IProps> = (props: IProps) => {
               component={'span'}
               noWrap={true}
             >
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                className={classes.menuButton}
+              >
+                <MenuIcon />
+              </IconButton>
               FTS Library
             </Typography>
           </Toolbar>
         </AppBar>
-        <Drawer
-          className={classes.drawer}
-          variant="permanent"
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-          anchor="left"
-        >
-          <Divider />
-          <List>
-            <ListItem button={true} key="home">
-              <ListItemIcon>
-                <Home />
-              </ListItemIcon>
-              <Link to="/">
-                <ListItemText primary="Home" />
-              </Link>
-            </ListItem>
-            <ListItem button={true} key="searchbookmarks">
-              <ListItemIcon>
-                <Search />
-              </ListItemIcon>
-              <Link to="/Search">
-                <ListItemText primary="Search" />
-              </Link>
-            </ListItem>
-          </List>
-        </Drawer>
+        <Hidden smUp implementation="css">
+          <Drawer
+            className={classes.drawer}
+            variant="temporary"
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            anchor="left"
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
         <div>
           <main className={classes.content}>{props.children}</main>
         </div>
